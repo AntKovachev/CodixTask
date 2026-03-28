c6a600e6dace43e54f6e1ae73f235e5fef6d4076
Anton Kovachev <anton.kovachev@frogsmile.com>
2026-03-28 11:05:35 +0200

    Build client form
    Fixes #8

--- take-home-ui/src/app/pages/client/client.component.ts
+++ take-home-ui/src/app/pages/client/client.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/client.model';
+import { CLIENT_FIELD_CONFIG, ClientFieldConfig } from '../../config/config';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  client: Client | null = null;
+  fields: ClientFieldConfig[] = CLIENT_FIELD_CONFIG;
+  isReady = false;
+  error = '';
 
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit(): void {
+    this.clientService.getClient().subscribe({
+      next: (data) => {
+        this.client = data;
+        this.isReady = true;
+      },
+      error: () => {
+        this.error = 'Failed to load client data. Please try again.';
+      }
+    });
+  }
 }
